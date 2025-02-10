1a5fcfdf1ce97d687a550368adb721204fb2ae1d
kajal0176 <100218847+kajal0176@users.noreply.github.com>
2025-02-10 09:22:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (53.48%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='0'&&s[i]<='9'){
+                if(ans.size()>0){
+                    ans.pop_back();
+                }
+            }
+            else{
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
