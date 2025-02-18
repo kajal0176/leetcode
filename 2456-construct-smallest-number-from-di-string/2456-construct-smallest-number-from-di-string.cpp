aa33ea9d1eddf598f569a97d4d53b5144d6bdfde
kajal0176 <100218847+kajal0176@users.noreply.github.com>
2025-02-18 11:19:30 +0530

    Time: 0 ms (100%), Space: 8.4 MB (27.59%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-void permutation(string s,vector<string>&ans,int ind){
-    if(ind==s.size()){
-        ans.push_back(s);
-        return;
-    }
-    for(int i=ind;i<s.size();i++){
-        swap(s[i],s[ind]);
-        permutation(s,ans,ind+1);
-        swap(s[i],s[ind]);
-    }
-}
     string smallestNumber(string pattern) {
-        string st="";
-        for(int i=1;i<=pattern.size()+1;i++){
-            st+=(to_string(i));
-        }
-        vector<string>temp;
-        permutation(st,temp,0);
-        for(auto it:temp){
-            string s=it;
-            int f=1;
-            for(int i=0;i<pattern.size();i++){
-                if(pattern[i]=='I'&&s[i]-'0'>=s[i+1]-'0'){
-                    f=0;
-                    break;
-                }
-                if(pattern[i]=='D'&&s[i]-'0'<=s[i+1]-'0'){
-                    f=0;
-                    break;
+        stack<int>st;
+        string ans="";
+        for(int i=0;i<=pattern.size();i++){
+            st.push(i+1);
+            if(i==pattern.size()||pattern[i]=='I'){
+                while(!st.empty()){
+                    ans+=to_string(st.top());
+                    st.pop();
                 }
             }
-            if(f==1){
-                return s;
-            }
         }
-        return "";
+        return ans;
     }
 };
