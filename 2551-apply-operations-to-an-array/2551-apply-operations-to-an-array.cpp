a36883f32663ae102ea7306e0f20446ea6385120
kajal0176 <100218847+kajal0176@users.noreply.github.com>
2025-03-01 09:11:33 +0530

    Time: 0 ms (100%), Space: 13.1 MB (87.59%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        stack<int>st;
-        for(int i=0;i<nums.size();i++){
-            if(!st.empty()&&st.top()==nums[i]&&nums[i]!=0){
-                int x=st.top();
-                st.pop();
-                st.push(x*2);
-                st.push(0);
+        int prev=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[prev]==nums[i]&&nums[i]!=0){
+                nums[prev]*=2;
+                nums[i]=0;
+                
             }
-           else{
-                st.push(nums[i]);
-            }
+                prev=i;
         }
         int ind=0;
-        while(!st.empty()){
-            int element=st.top();
-            st.pop();
-            if(element!=0){
-                nums[ind]=element;
-                ind++;
-            }
+       
+        for(int i=0;i<nums.size();i++){
+           if(nums[i]!=0){
+            nums[ind]=nums[i];
+            ind++;
+           }
         }
-        reverse(nums.begin(),nums.begin()+ind);
         for(int i=ind;i<nums.size();i++){
             nums[i]=0;
         }
