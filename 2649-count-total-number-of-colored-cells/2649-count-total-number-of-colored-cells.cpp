c81b547332be7079045af3ce0888882a92ec20af
kajal0176 <100218847+kajal0176@users.noreply.github.com>
2025-03-05 09:58:43 +0530

    Time: 2 ms (25.62%), Space: 7.7 MB (99.69%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if(n==1)return 1;
-        if(n==2)return 5;
-        long long ans=coloredCells(n-1);
-        long long added=4+(n-2)*4;
-        return ans+added;
+       if(n==1)return 1;
+        long long ans=5;
+        //in each step multiple of 4 no. of cell is being added eg 4,8,12......
+        for(int i=3;i<=n;i++){
+          long long added=4+(i-2)*4;
+          ans+=added;
+        }
+        return ans;
     }
 };
