cdfe5043568a1da1c4c835a562cad810de5e0d70
kajal0176 <100218847+kajal0176@users.noreply.github.com>
2025-03-10 11:16:50 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.42%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int ans=0;
+        int cnt=1;
+        for(int i=1;i<word.size();i++){
+            if(word[i]==word[i-1]){
+                cnt++;
+            }
+            else{
+                ans+=(cnt-1);
+                cnt=1;
+            }
+        }
+        ans+=(cnt-1);
+        
+        return ans+1;
+    }
+};
